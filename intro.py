9ab55c4b359513d6281231e911d5edf800b335f9
allenzpierre <pieall25@ecolecatholique.ca>
2021-01-15 01:52:37 +0000

    Initial commit

---
+++ intro.py
@@ -0,0 +1,9 @@
+
+alien = Actor('alien')
+
+WIDTH = 500
+HEIGHT = alien.height + 20
+
+def draw():
+  screen.clear()
+  alien.draw()
---
+++ main.py
@@ -0,0 +1,28 @@
+import pgzrun
+
+WIDTH = 800
+HEIGHT = 500
+speed = 1
+
+alien = Actor("alien.png")
+
+def draw():
+  screen.fill("red")
+  alien.draw()
+  if alien.y < 0:
+    print("game over")
+
+def update():
+  alien.x = alien.x + speed 
+
+def on_mouse_down(pos):
+  if alien.collidepoint(pos):
+    set_alien_hurt()
+  
+def set_alien_normal():
+  alien.image = "alien"
+
+def set_alien_hurt():
+  alien.image = "alien_hurt"
+
+pgzrun.go()
